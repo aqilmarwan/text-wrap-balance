d4c98c3b6b838320611a9aebeff01fa0865be4ce
aqilmarwan <aqilmarwanazhar@gmail.com>
2023-12-21 01:42:17 +0000

    format

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,10 +12,6 @@ const Button = () => {
         setCurrentStyle(style);
     };
 
-    const setDefaultValue = (content: string) => {
-        setTextContent(content);
-    };
-
     const [textContent, setTextContent] = useState(
         "Default text content here..."
     );
@@ -24,41 +20,37 @@ const Button = () => {
         default: `.h5 {
         font-size: 1.25rem;
         margin-bottom: 1rem;
-        }`,
-        auto: `.h5-with-auto {
+}`,
+        auto: `.h5 {
         font-size: 1.25rem;
         margin-bottom: 1rem;
         text-wrap: auto;
-        }`,
-        balance: `.h5-with-balance {
+}`,
+        balance: `.h5 {
         font-size: 1.25rem;
         margin-bottom: 1rem;
         text-wrap: balance;
-        }`,
-        stable: `.h5-with-stable {
+}`,
+        stable: `.h5 {
         font-size: 1.25rem;
         margin-bottom: 1rem;
         text-wrap: stable;
-        }`,
-        pretty: `.h5-with-pretty {
+}`,
+        pretty: `.h5 {
         font-size: 1.25rem;
         margin-bottom: 1rem;
         text-wrap: pretty;
-        }`,
+}`,
     };
 
     useEffect(() => {
-        // You can customize this logic based on your requirements
-        // For example, you can reload the editor by changing its key prop
     }, [textWrapStyle]);
-
-    const formattedCSS = textWrapStyle ? monacoEditorStyles[textWrapStyle] : monacoEditorStyles.default;
 
     return (
         <div className='flex flex-col items-center'>
             <div className='mt-2 mb-2 font-mono text-slate-600'>
                 <span aria-hidden="true">&darr;{' '}</span>
-                Choose one 'text-wrap-style' property
+                Choose one 'text-wrap-style' property.
                 <span aria-hidden="true">{' '}&darr;</span>
             </div>
             <div className='flex flex-row cursor-pointer items-center relative'>
@@ -76,6 +68,11 @@ const Button = () => {
                         <button type="button" onClick={() => handleStyleChange('pretty')} className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
                             pretty
                         </button>
+                        <div className='ml-2'>
+                            <button type="button" onClick={() => handleStyleChange('')} className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent hover:bg-blue-500 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded-s-lg rounded-e-lg">
+                                normal text
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -89,23 +86,23 @@ const Button = () => {
                     </h2>
                     <>
                         <h5 className="max-w-3xl text-md font-medium md:text-md lg:text-md">
-                            {textWrapStyle === 'auto' && (
+                            {currentStyle === 'auto' && (
                                 <p className='h5-with-auto'>
                                     AI is revolutionizing technology, allowing machines to emulate human intelligence in learning, problem-solving, and decision-making. Through advanced algorithms and machine learning, AI excels at recognizing patterns, including image and speech recognition, and providing personalized recommendations.                                </p>
                             )}
-                            {textWrapStyle === 'balance' && (
+                            {currentStyle === 'balance' && (
                                 <p className='h5-with-balance'>
                                     AI is revolutionizing technology, allowing machines to emulate human intelligence in learning, problem-solving, and decision-making. Through advanced algorithms and machine learning, AI excels at recognizing patterns, including image and speech recognition, and providing personalized recommendations.                                </p>
                             )}
-                            {textWrapStyle === 'stable' && (
+                            {currentStyle === 'stable' && (
                                 <p className='h5-with-stable'>
                                     AI is revolutionizing technology, allowing machines to emulate human intelligence in learning, problem-solving, and decision-making. Through advanced algorithms and machine learning, AI excels at recognizing patterns, including image and speech recognition, and providing personalized recommendations.                                </p>
                             )}
-                            {textWrapStyle === 'pretty' && (
+                            {currentStyle === 'pretty' && (
                                 <p className='h5-with-pretty'>
                                     AI is revolutionizing technology, allowing machines to emulate human intelligence in learning, problem-solving, and decision-making. Through advanced algorithms and machine learning, AI excels at recognizing patterns, including image and speech recognition, and providing personalized recommendations.                                </p>
                             )}
-                            {!textWrapStyle && (
+                            {!currentStyle && (
                                 <p className='h5'>
                                     AI is revolutionizing technology, allowing machines to emulate human intelligence in learning, problem-solving, and decision-making. Through advanced algorithms and machine learning, AI excels at recognizing patterns, including image and speech recognition, and providing personalized recommendations.                                </p>
                             )}
@@ -119,27 +116,27 @@ const Button = () => {
                 </h2>
             </div>
             <>
-                {textWrapStyle && (
+                {currentStyle && (
                     <MonacoEditor
-                        key={monacoEditorStyles[textWrapStyle]}
+                        key={monacoEditorStyles[currentStyle]}
                         className="monaco-editor mb-12"
                         height="40vh"
                         width="40%"
                         theme="vs-dark"
                         language="css"
-                        defaultValue={monacoEditorStyles[textWrapStyle]}
+                        defaultValue={monacoEditorStyles[currentStyle]}
                         options={{
-                            minimap: { enabled: false }, // Disable minimap if you want
-                            scrollBeyondLastLine: false,  // Disable scrolling beyond the last line
-                            wordWrap: 'on',  // Enable word wrap
-                            lineNumbers: 'on', // Enable line numbers
-                            glyphMargin: false, // Disable the glyph margin (squiggly lines)
-                            contextmenu: false, // Disable the context menu
-                            rulers: [], // Disable rulers
+                            minimap: { enabled: false },
+                            scrollBeyondLastLine: false,
+                            wordWrap: 'on',
+                            lineNumbers: 'on',
+                            glyphMargin: false,
+                            contextmenu: false,
+                            rulers: [],
                         }}
                     />
                 )}
-                {(!textWrapStyle || textWrapStyle === 'default') && (
+                {!currentStyle && (
                     <MonacoEditor
                         key={monacoEditorStyles.default}
                         className="monaco-editor mb-12"
@@ -149,13 +146,13 @@ const Button = () => {
                         language="css"
                         defaultValue={monacoEditorStyles.default}
                         options={{
-                            minimap: { enabled: false }, // Disable minimap if you want
-                            scrollBeyondLastLine: false,  // Disable scrolling beyond the last line
-                            wordWrap: 'on',  // Enable word wrap
-                            lineNumbers: 'on', // Enable line numbers
-                            glyphMargin: false, // Disable the glyph margin (squiggly lines)
-                            contextmenu: false, // Disable the context menu
-                            rulers: [], // Disable rulers
+                            minimap: { enabled: false },
+                            scrollBeyondLastLine: false,
+                            wordWrap: 'on',
+                            lineNumbers: 'on',
+                            glyphMargin: false,
+                            contextmenu: false,
+                            rulers: [],
                         }}
                     />
                 )}
